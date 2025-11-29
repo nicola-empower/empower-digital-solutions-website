faca825a37baa743797fba08e8c9d08062d218a8
Nicola Berry <nicola@empowervaservices.co.uk>
2025-11-29 21:57:36 +0000

    latest overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()]
+    },
 
-  integrations: [react()],
-  output: 'server',
-  adapter: vercel(),
+    integrations: [react()],
+    output: 'server',
+    adapter: vercel(),
 });
